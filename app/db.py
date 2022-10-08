003c740b03c45561792de4808b2f30550f572ca5
chodorb <baz.chodor@gmail.com>
2022-10-08 16:07:36 +0200

    db fix

--- app/db.py
+++ app/db.py
@@ -11,7 +11,7 @@ password = os.environ.get("DB_PASSWORD")
 name = os.environ.get("DB_NAME")
 
 
-DATABASE_URI = "postgresql://{}:{}@localhost/{}".format(user,password,name)
+DATABASE_URI = "postgresql://{}:{}@postgresdb/{}".format(user,password,name)
 engine = create_engine(DATABASE_URI)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False,bind=engine)
 Base = declarative_base()
